5864cc5a1d7d33fe9af7580488c5b1d3390d3877
angela13st <49033470+angela13st@users.noreply.github.com>
2023-06-27 23:58:01 +0200

    ws1
    ws1

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import CategoryItem from "./CategoryItem";
+import { categories } from "../data";
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+`;
+const Categories = () => {
+  return <Container>
+      {categories.map(item=>(
+        <CategoryItem item={item}/>
+        ))}
+        </Container>;
+};
+
+export default Categories;
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://www.myflowerroom.com.au/wp-content/uploads/2019/07/C1ADA881-8676-449B-B212-FA4750866D0D-500x500.jpeg",
+        title:"Savršeno cvijeće i darovi za svaku prigodu",
+        desc:"Prekrasno aranžirano da oduševi i iznenadi svakoga na njegov poseban dan.",
+        bg:"f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://cdn11.bigcommerce.com/s-tf2vn6/product_images/uploaded_images/event-fs.jpg",
+        title:"PERFECT WEDDING",
+        desc:"Učinimo vaš poseban dan savršenim. Dekoracije sala, vjenčani buketi, narukvice za goste... ",
+        bg:"fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://media1.thehungryjpeg.com/thumbs2/ori_3933412_44jj9bs97jtl9gna7qwkx9p8xlgnatg152lrg65y_summer-sale-poster-natural-background-for-poster.jpg",
+        title:"SUMMER SALE",
+        desc:"Istražite naše popuste.",
+        bg:"fbf0f4",
+    },
+   
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://flora94.weebly.com/uploads/1/2/6/7/126708403/fb-img-1564047938419.jpg",
+        title:"BUKETI",
+    },
+    {
+        id: 2,
+        img: "https://flora94.weebly.com/uploads/1/2/6/7/126708403/fb-img-1564048109507.jpg",
+        title:"ARANŽMANI",
+    },
+    {
+        id: 3,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4L3fQ-7O5-V0RcwqKLV37dVg8wfGUk1u0Nu2ZmnMNXaG_inNI7vS6T0JAxp8uCDRO4Fk&usqp=CAU",
+        title:"PRIGODNO",
+    }
+   
+]
+
